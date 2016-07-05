2815bd77a619081aa11e819acd6492aa4f84f95c
Michael Miller <icy.arctic.fox@gmail.com>
2016-07-05 12:21:28 -0600

    Add tests for Point4

---
+++ Leaf/Leaf.Tests/Unit/Types/Point4Tests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using Leaf.Types;
+
+namespace Leaf.Tests.Unit.Types
+{
+    [TestFixture]
+    public class Point4Tests
+    {
+        /// <summary>
+        /// Test that the X property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestXGetter()
+        {
+            const int x = 3, y = 4, z = 5, w = 6;
+            var point = new Point4(x, y, z, w);
+            Assert.AreEqual(x, point.X);
+        }
+
+        /// <summary>
+        /// Test that the Y property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestYGetter()
+        {
+            const int x = 3, y = 4, z = 5, w = 6;
+            var point = new Point4(x, y, z, w);
+            Assert.AreEqual(y, point.Y);
+        }
+
+        /// <summary>
+        /// Test that the Z property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestZGetter()
+        {
+            const int x = 3, y = 4, z = 5, w = 6;
+            var point = new Point4(x, y, z, w);
+            Assert.AreEqual(z, point.Z);
+        }
+
+        /// <summary>
+        /// Test that the W property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestWGetter()
+        {
+            const int x = 3, y = 4, z = 5, w = 6;
+            var point = new Point4(x, y, z, w);
+            Assert.AreEqual(w, point.W);
+        }
+    }
+}
