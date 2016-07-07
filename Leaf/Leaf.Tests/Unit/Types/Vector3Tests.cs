6642d3a5f62ffafeded4634b4d5d4f5f54f82706
Michael Miller <icy.arctic.fox@gmail.com>
2016-07-06 20:21:51 -0600

    Add tests for Vector3

---
+++ Leaf/Leaf.Tests/Unit/Types/Vector3Tests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Leaf.Types;
+
+namespace Leaf.Tests.Unit.Types
+{
+    [TestFixture]
+    public class Vector3Tests
+    {
+        /// <summary>
+        /// Test that the X property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestXGetter()
+        {
+            const float x = 3f, y = 4f, z = 5f;
+            var vector = new Vector3(x, y, z);
+            Assert.AreEqual(x, vector.X);
+        }
+
+        /// <summary>
+        /// Test that the Y property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestYGetter()
+        {
+            const float x = 3f, y = 4f, z = 5f;
+            var vector = new Vector3(x, y, z);
+            Assert.AreEqual(y, vector.Y);
+        }
+
+        /// <summary>
+        /// Test that the Z property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestZGetter()
+        {
+            const float x = 3f, y = 4f, z = 5f;
+            var vector = new Vector3(x, y, z);
+            Assert.AreEqual(z, vector.Z);
+        }
+    }
+}
