981afe9fa2e1e9109b39237969a8a90de18fc003
Michael Miller <icy.arctic.fox@gmail.com>
2016-07-06 20:25:30 -0600

    Add tests for Vector4

---
+++ Leaf/Leaf.Tests/Unit/Types/Vector4Tests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using Leaf.Types;
+
+namespace Leaf.Tests.Unit.Types
+{
+    [TestFixture]
+    public class Vector4Tests
+    {
+        /// <summary>
+        /// Test that the X property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestXGetter()
+        {
+            const float x = 3f, y = 4f, z = 5f, w = 6f;
+            var vector = new Vector4(x, y, z, w);
+            Assert.AreEqual(x, vector.X);
+        }
+
+        /// <summary>
+        /// Test that the Y property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestYGetter()
+        {
+            const float x = 3f, y = 4f, z = 5f, w = 6f;
+            var vector = new Vector4(x, y, z, w);
+            Assert.AreEqual(y, vector.Y);
+        }
+
+        /// <summary>
+        /// Test that the Z property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestZGetter()
+        {
+            const float x = 3f, y = 4f, z = 5f, w = 6f;
+            var vector = new Vector4(x, y, z, w);
+            Assert.AreEqual(z, vector.Z);
+        }
+
+        /// <summary>
+        /// Test that the W property's getter returns the correct value.
+        /// </summary>
+        [Test]
+        public void TestWGetter()
+        {
+            const float x = 3f, y = 4f, z = 5f, w = 6f;
+            var vector = new Vector4(x, y, z, w);
+            Assert.AreEqual(w, vector.W);
+        }
+    }
+}
