0b14e9e99d362a7d746353f885d2e8aaf66b819f
Michael Miller <icy.arctic.fox@gmail.com>
2016-07-12 11:23:49 -0600

    Add base node class

---
+++ Leaf/Leaf/Nodes/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Leaf.Nodes
+{
+    /// <summary>
+    /// Base class for all node types.
+    /// </summary>
+    public abstract class Node
+    {
+        /// <summary>
+        /// Writes the data of the node to a stream.
+        /// </summary>
+        /// <param name="writer">Writer to use for putting data in the stream.</param>
+        public abstract void Write(BinaryWriter writer);
+    }
+}
