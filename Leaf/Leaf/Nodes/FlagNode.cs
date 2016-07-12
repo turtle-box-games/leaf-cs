b58c49f52fd2e7dfbfb3734d6cc3bc648af61187
Michael Miller <icy.arctic.fox@gmail.com>
2016-07-12 11:38:04 -0600

    Add flag node class
    Most other node types will have this exact structure.

---
+++ Leaf/Leaf/Nodes/FlagNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Leaf.Nodes
+{
+    /// <summary>
+    /// Stores a boolean value.
+    /// </summary>
+    public class FlagNode : Node
+    {
+        /// <summary>
+        /// Gets and sets the value of the node.
+        /// </summary>
+        public bool Value
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Creates a new node.
+        /// </summary>
+        /// <param name="value">Value of the node.</param>
+        public FlagNode(bool value)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a new node by reading its contents from a stream.
+        /// </summary>
+        /// <param name="reader">Reader used to pull data from the stream.</param>
+        /// <returns>Newly constructed node.</returns>
+        internal FlagNode Read(BinaryReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes the contents of the node to a stream.
+        /// </summary>
+        /// <param name="writer">Writer used to put data in the stream.</param>
+        internal override void Write(BinaryWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
