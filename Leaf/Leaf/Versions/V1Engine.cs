6f0c39989dae5073fd9c3954f9b53bc35bf5656b
Michael Miller <icy.arctic.fox@gmail.com>
2016-08-29 17:18:44 -0600

    Add structure for V1 engine and header

---
+++ Leaf/Leaf/Versions/V1Engine.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Leaf.Nodes;
+
+namespace Leaf.Versions
+{
+    /// <summary>
+    /// First version of the node engine.
+    /// </summary>
+    public class V1Engine : Engine
+    {
+        /// <summary>
+        /// Numerical version used to distinguish this engine from others.
+        /// For this engine, the value is 1.
+        /// </summary>
+        public override int Version { get; }
+
+        /// <summary>
+        /// Create a header that contains the node structure information that the engine can use later after serialization.
+        /// </summary>
+        /// <return>Version 1 header containing information on how the nodes are structured.</return>
+        /// <seealso cref="V1Header"/>
+        internal override Header CreateHeader()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reads a root node (structure) from a stream.
+        /// </summary>
+        /// <param name="reader">Reader used to pull data from the stream.</param>
+        /// <returns>Root node (structure).</returns>
+        internal override Node ReadStructure(BinaryReader reader)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes a root node (structure) to a stream.
+        /// </summary>
+        /// <param name="writer">Writer used to put data into the stream.</param>
+        /// <param name="node">Root node (structure).</param>
+        internal override void WriteStructure(BinaryWriter writer, Node node)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Leaf/Leaf/Versions/V1Header.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Leaf.Versions
+{
+    /// <summary>
+    /// First version of the node header.
+    /// </summary>
+    public class V1Header : Header
+    {
+        /// <summary>
+        /// Numerical ID used to distinguish the header and engine type.
+        /// For this header version, the value is 1.
+        /// </summary>
+        public override int Version { get; }
+
+        /// <summary>
+        /// Serializes the header and writes it to a stream.
+        /// </summary>
+        /// <param name="writer">Writer used to put data in the stream.</param>
+        internal override void Write(BinaryWriter writer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
