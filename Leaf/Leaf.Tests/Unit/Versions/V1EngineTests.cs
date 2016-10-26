b134683e6e3152b2abd99daa891a1b24713d9324
Michael Miller <icy.arctic.fox@gmail.com>
2016-10-26 05:20:43 -0600

    Basic test for V1Engine

---
+++ Leaf/Leaf.Tests/Unit/Versions/V1EngineTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Leaf.Versions;
+
+namespace Leaf.Tests.Unit.Versions
+{
+    [TestFixture]
+    public class V1EngineTests
+    {
+        /// <summary>
+        /// Verify that the version reported by the engine is 1.
+        /// </summary>
+        [Test]
+        public void TestVersion()
+        {
+            var engine = new V1Engine();
+            Assert.AreEqual(1, engine.Version);
+        }
+    }
+}
