7bfcf628cab47e31ff3852d6184a31978ed0574a
Michael Miller <icy.arctic.fox@gmail.com>
2017-03-16 19:49:51 -0600

    Remove serialization from container

--- Leaf/Leaf.Tests/ContainerTests.cs
+++ Leaf/Leaf.Tests/ContainerTests.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.IO;
 using Leaf.Nodes;
-using Leaf.Versions;
 using NUnit.Framework;
 
 namespace Leaf.Tests
@@ -22,30 +20,6 @@ namespace Leaf.Tests
         }
 
         /// <summary>
-        /// Verify that the custom engine constructor throws a <see cref="ArgumentNullException"/> when the root node is null.
-        /// </summary>
-        [Test]
-        public void TestNullRootEngine()
-        {
-            Assert.Throws<ArgumentNullException>(() =>
-            {
-                new Container(null, new V1Engine());
-            });
-        }
-
-        /// <summary>
-        /// Verify that the constructor throws a <see cref="ArgumentNullException"/> when the engine is null.
-        /// </summary>
-        [Test]
-        public void TestNullEngine()
-        {
-            Assert.Throws<ArgumentNullException>(() =>
-            {
-                new Container(new Int32Node(50), null);
-            });
-        }
-
-        /// <summary>
         /// Verify that the root node property is set properly.
         /// </summary>
         [Test]
@@ -54,38 +28,6 @@ namespace Leaf.Tests
             var root = new Int32Node(50);
             var container = new Container(root);
             Assert.AreEqual(root, container.Root);
-        }
-
-        /// <summary>
-        /// Check that something (anything) is written to the stream.
-        /// </summary>
-        [Test]
-        [Ignore("Not implemented yet")]
-        public void TestWriteAny()
-        {
-            var root = new Int32Node(50);
-            var container = new Container(root);
-            using (var stream = new MemoryStream())
-            {
-                container.Write(stream);
-                Assert.Greater(stream.Position, 0);
-            }
-        }
-
-        /// <summary>
-        /// Check that the stream stays open after writing to it.
-        /// </summary>
-        [Test]
-        [Ignore("Not implemented yet")]
-        public void TestWriteKeepOpen()
-        {
-            var root = new Int32Node(50);
-            var container = new Container(root);
-            using (var stream = new MemoryStream())
-            {
-                container.Write(stream);
-                Assert.IsTrue(stream.CanRead);
-            }
         }
     }
 }
--- Leaf/Leaf/Container.cs
+++ Leaf/Leaf/Container.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.IO;
-using System.Text;
-using Leaf.IO;
 using Leaf.Nodes;
-using Leaf.Versions;
 
 namespace Leaf
 {
@@ -13,11 +9,6 @@ namespace Leaf
     /// </summary>
     public class Container
     {
-        /// <summary>
-        /// Engine used to parse the node structure and header for the container.
-        /// </summary>
-        private readonly Engine _engine;
-
         /// <summary>
         /// Access to the root node of the structure.
         /// </summary>
@@ -33,88 +24,7 @@ namespace Leaf
             if (root == null)
                 throw new ArgumentNullException(nameof(root));
 
-            Root    = root;
-            _engine = new V1Engine();
+            Root = root;
         }
-
-        /// <summary>
-        /// Creates a container for a node with a specific storage engine.
-        /// </summary>
-        /// <param name="root">Root node containing all data in the structure.</param>
-        /// <param name="engine">Engine that understands and serializes the node's structure.</param>
-        public Container(Node root, Engine engine)
-        {
-            if (root == null)
-                throw new ArgumentNullException(nameof(root));
-            if (engine == null)
-                throw new ArgumentNullException(nameof(engine));
-
-            Root    = root;
-            _engine = engine;
-        }
-
-        #region Serialization
-
-        /// <summary>
-        /// Flag indicating whether or not to use big-endian during serialization.
-        /// </summary>
-        private const bool BigEndian = true;
-
-        /// <summary>
-        /// Bytes that prefix the contents of every container.
-        /// The signature spells out LEAF in ASCII.
-        /// </summary>
-        private static readonly byte[] Signature = { 0x4C, 0x45, 0x41, 0x46 };
-
-        /// <summary>
-        /// Text encoding used for serialization.
-        /// </summary>
-        private static readonly Encoding StreamEncoding = Encoding.UTF8;
-
-        #region Read
-
-        /// <summary>
-        /// Reads a node structure from a stream.
-        /// </summary>
-        /// <param name="input">Stream to read a container from.</param>
-        public static Container Read(Stream input)
-        {
-            using (var reader = new EndianAwareBinaryReader(input, BigEndian, StreamEncoding, true))
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        #endregion
-        #region Write
-
-        /// <summary>
-        /// Writes the contents of the container and its structure to a stream.
-        /// </summary>
-        /// <param name="output">Stream to write the container to.</param>
-        public void Write(Stream output)
-        {
-            var header = _engine.CreateHeader(Root);
-            using (var writer = new EndianAwareBinaryWriter(output, BigEndian, StreamEncoding, true))
-            {
-                WriteSignature(writer, header);               // Write signature.
-                header.Write(writer);                         // Write header.
-                _engine.WriteStructure(writer, header, Root); // Write structure.
-            }
-        }
-
-        /// <summary>
-        /// Writes the Leaf signature and version to a stream.
-        /// </summary>
-        /// <param name="writer">Writer used to put data on the stream.</param>
-        /// <param name="header">Header data created by the engine.</param>
-        private static void WriteSignature(BinaryWriter writer, Header header)
-        {
-            writer.Write(Signature);
-            writer.Write(header.Version);
-        }
-
-        #endregion
-        #endregion
     }
 }
