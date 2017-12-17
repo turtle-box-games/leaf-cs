bfcbe428421ef79e4bdfe645890fc68a0b291324
Michael Miller <icy.arctic.fox@gmail.com>
2017-12-17 11:35:23 -0700

    Update BinaryFormatSerializer to produce and consume a byte array

--- Leaf/Leaf/Serialization/BinaryFormatSerializer.cs
+++ Leaf/Leaf/Serialization/BinaryFormatSerializer.cs
@@ -7,9 +7,8 @@ namespace Leaf.Serialization
 {
     /// <summary>
     /// Serializes node data to a compact, non-human readable format.
-    /// This format is ideal for saving in a compact format.
     /// </summary>
-    public class BinaryFormatSerializer : IFormatSerializer
+    public class BinaryFormatSerializer : IFormatSerializer<byte[]>
     {
         /// <summary>
         /// Flag indicating whether or not to use big-endian during serialization.
@@ -22,32 +21,34 @@ namespace Leaf.Serialization
         private static readonly Encoding StreamEncoding = Encoding.UTF8;
 
         /// <summary>
-        /// Write node data to a stream.
+        /// Generate a byte array containing the node data from a container.
         /// </summary>
-        /// <param name="container">Container holding the root node and associated data
-        /// to be written to the stream.</param>
-        /// <param name="output">Output stream to write data to.</param>
-        public void Serialize(Container container, Stream output)
+        /// <param name="container">Container holding the root node and associated data to be serialized.</param>
+        /// <returns>Output containing the serialized node data.</returns>
+        public byte[] Serialize(Container container)
         {
             var root   = container.Root;
             var header = new BinaryFormatHeader(root.Type, root.Version);
-            using(var writer = new EndianAwareBinaryWriter(output, BigEndian, StreamEncoding, true))
+            using(var stream = new MemoryStream())
+            using(var writer = new EndianAwareBinaryWriter(stream, BigEndian, StreamEncoding))
             {
                 var serializer = new BinaryNodeSerializer(writer);
                 header.Write(writer);
                 root.Serialize(serializer);
+                return stream.ToArray();
             }
         }
 
         /// <summary>
-        /// Read node data from a stream.
+        /// Create a container from serialized node data.
         /// </summary>
-        /// <param name="input">Input stream to read data from.</param>
-        /// <returns>Container holding the root node and associated data read from the stream.</returns>
-        public Container Deserialize(Stream input)
+        /// <param name="input">Byte array containing previously serialized node data.</param>
+        /// <returns>Container holding the root node and associated data pull from the input.</returns>
+        public Container Deserialize(byte[] input)
         {
             Node root;
-            using(var reader = new EndianAwareBinaryReader(input, BigEndian, StreamEncoding, true))
+            using(var stream = new MemoryStream(input))
+            using(var reader = new EndianAwareBinaryReader(stream, BigEndian, StreamEncoding))
             {
                 var header = BinaryFormatHeader.Read(reader);
                 var serializer = new BinaryNodeSerializer(reader);
